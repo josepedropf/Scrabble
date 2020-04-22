7709530968af8e20e743791f6cfd46d7caee719d
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-04-22 11:15:16 +0100

    Add files via upload

---
+++ ScrabbleBoardBuilder.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <windows.h>
+#include<limits>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
+
+int boardsize = 1;
+
