005bbf28a95a957497aa68defc015c40d22cddad
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-04 19:47:17 +0100

    Add files via upload

---
+++ BoardBuilder.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Board.h"
+using namespace std;
+
+int main()
+{
+    int mode = 1;
+    bool end = false;
+    string rr;
+    string savefile;
+    vector<string> possible_words;
+    vector<int> npos(2, 0);
+    vector<vector<char>> gameboard;
+    int boardsize = 0;
+    Board board;
+
+    board.GetWords(possible_words, gameboard);
+
+    while (!end)
+    {
+
+        board.PickSize(boardsize);
+        vector<char> line;
+        line.clear();
+        gameboard.clear();
+
+        for (int i = 0; i < boardsize; i++)
+        {
+            line.push_back('0');
+        }
+
+        for (int i = 0; i < boardsize; i++)
+        {
+            gameboard.push_back(line);
+        }
+
+        board.DrawBoard(boardsize, gameboard);
+
+        cout << endl << "Modes: " << endl << endl << "O -> Exit" << endl
+             << "1 -> Choose the initial coordinates for Random Words" << endl << "2 -> Totally Random Board" << endl
+             << "3 -> Choose the initial Coordinates and the Words" << endl;
+        cout << "Choose the mode of Board Building (Input one of the numbers above): ";
+        while (1)
+        {
+            cin >> mode;
+            if (cin.fail() || mode < 0 || mode > 3)
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << endl << "Invalid Mode!" << endl;
+                cout << endl << "Modes: " << endl << endl << "O -> Exit" << endl
+                     << "1 -> Choose the initial Coordinates for Random Words" << endl << "2 -> Totally Random Board"
+                     << endl << "3 -> Choose the initial Coordinates and the Words" << endl;
+                cout << "Choose the mode of Board Building (Input one of the numbers above): ";
+            }
+            else
+                break;
+        }
+
+        switch (mode)
+        {
+            case 0:{
+                break;
+            }
+            case 1: {
+                board.Coordinates(gameboard, boardsize, npos, possible_words, mode);
+                break;
+            }
+            case 2: {
+                board.RandomBoard(boardsize, npos, gameboard, possible_words);
+                break;
+            }
+            case 3:{
+                board.Coordinates(gameboard, boardsize, npos, possible_words, mode);
+                break;
+            }
+        }
+        cout << "Do you want to save the Board in a File [Y for yes || N for no] ? ";
+        while(1)
+        {
+            cin >> savefile;
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Invalid Input!" << endl;
+                cout << "Do you want to save the Board in a File [Y for yes || N for no] ? ";
+            }
+            else
+            {
+                board.Lowerstr(savefile);
+                if (savefile == "y")
+                {
+                    string filename;
+                    cout << "Name of the File (without .txt) : ";
+                    while(1)
+                    {
+                        cin >> filename;
+                        if (cin.fail())
+                        {
+                            cin.clear();
+                            cin.ignore(1000, '\n');
+                            cout << "Invalid Input!" << endl;
+                            cout << "Name of the File (without .txt) : ";
+                        }
+                        else
+                        {
+                            board.FormatFile(boardsize, filename, gameboard);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                    break;
+            }
+        }
+
+        cout << "Do you want to make a different Board [Y for yes || N for no] ? ";
+        while(1)
+        {
+            cin >> rr;
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Invalid Input!" << endl;
+                cout << "Do you want to make a different Board [Y for yes || N for no] ? ";
+            }
+            else
+            {
+                board.Lowerstr(rr);
+                if (rr == "y")
+                    break;
+                else
+                {
+                    end = true;
+                    break;
+                }
+            }
+        }
+    }
+}
