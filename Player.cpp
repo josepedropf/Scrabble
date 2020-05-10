31bcb75ee75d88f7832d661ca5b3c214c0ca3fee
jfred24 <58302405+jfred24@users.noreply.github.com>
2020-05-11 00:53:58 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,3 +5,25 @@
 #include <ctime>
 #include <string>
 using namespace std;
+void Player::getPlayers() {
+    while(true)
+    {
+        cout << "What will be the number of players? [2-4]";
+        cin >> nplayers;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << endl << "Invalid input!" << endl;
+        }
+
+        if (nplayers > 4 || nplayers < 2) {
+            cout << endl << "Invalid input!" << endl;
+        }
+
+        else
+            break;
+    }
+
+
+}
