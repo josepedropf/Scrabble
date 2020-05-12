c85a803a3f0b7090a7203b7e43e1c61d23ef4371
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-12 18:06:47 +0100

    Update ScrabbleJunior.cpp

--- ScrabbleJunior.cpp
+++ ScrabbleJunior.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 #include <vector>
+#include <windows.h>
 #include "ScrabbleBoard.h"
 #include "Pool.h"
 #include "Player.h"
+
 using namespace std;
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
 
 int main()
 {
@@ -14,6 +34,10 @@ int main()
     ScrabbleBoard sb;
     string filen;
     string path = "C:\\Users\\MSI\\CLionProjects\\ScrabbleBoardBuilder\\cmake-build-debug\\";
+    unsigned int text_color = 1;
+    int turn = 1;
+    int initiald = 7;
+    sb.SetColor(text_color);
     cout << endl << "Welcome to Scrabble Junior!" << endl;
     cout << "What's the name of the Board File (without .txt) : ";
     while(true)
@@ -37,7 +61,32 @@ int main()
         else
             break;
     }
-    sb.OpenBoard(filen, sb.boardsize, sb.gameboard);
-    sb.DrawGameBoard(sb.boardsize, sb.gameboard);
-    pool.InitialPool(sb.gameboard, sb.boardsize, pool.pool);
+    sb.OpenBoard(filen);
+    sb.DrawGameBoard(text_color);
+    pool.InitialPool(sb.gameboard, sb.boardsize);
+    plr.GetPlayers();
+    int cn = 1;
+    while(true)
+    {
+        cout << endl << "Do you want to customize your names? [0 for No || Any other number for Yes] ";
+        cin >> cn;
+        if(cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << endl << "Invalid Answer (it has to be a Number)! " << endl;
+        }
+        else
+            break;
+    }
+
+    if (cn != 0)
+        plr.CustomPlayerNames();
+
+    if (pool.pool.size() >= 7 * plr.nplayers)
+        initiald = 7;
+    else
+        initiald = pool.pool.size() / plr.nplayers;
+    pool.InitialDraw(initiald, plr.nplayers);
+
 }
