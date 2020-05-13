e2dc4b3f72164e3a0a1c553e4e81acccd7ffa1c0
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-13 17:06:36 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -11,6 +11,7 @@ public:
     string pname2 = "Player 2";
     string pname3 = "Player 3";
     string pname4 = "Player 4";
+    int scorep1 = 0, scorep2 = 0, scorep3 = 0, scorep4 = 0;
 
     void GetPlayers();
     void CustomPlayerNames();
