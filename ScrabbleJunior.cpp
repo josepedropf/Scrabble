1034384c612ca8da03d884e460bbaff750052719
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-14 17:58:39 +0100

    Update ScrabbleJunior.cpp

--- ScrabbleJunior.cpp
+++ ScrabbleJunior.cpp
@@ -38,206 +38,237 @@ int main()
     string path = "C:\\Users\\MSI\\CLionProjects\\BoardBuilder\\cmake-build-debug\\";
     //string path = "C:\\Users\\Utilizador\\CLionProjects\\ScrabbleBoardBuilder\\cmake-build-debug\\";
     unsigned int text_color = 1;
-    int turn = 1, turncount = 0;
+    int turn = 1, turncount = 0, rranswer = 1;
     int initiald = 7, line = 0, col = 0;
     string scoord;
-    bool playing = true, validp = false;
+    bool playing = true, validp = false, rr = true;
     sb.SetColor(text_color);
-    cout << endl << "Welcome to Scrabble Junior!" << endl;
-    cout << "What's the name of the Board File (without .txt) : ";
-    while(true)
+    while (rr)
     {
-        cin >> filen;
-        if (cin.fail())
+        if (rranswer == 0)
         {
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << endl << "Invalid File Name!" << endl;
-            cout << "What's the name of the Board File (without .txt) : ";
+            sb.gameboard.clear();
+            playing = true;
+            sb.scorechips = 0;
+            pool.pool.clear();
+            pool.charp1.clear();
+            pool.charp2.clear();
+            pool.charp3.clear();
+            pool.charp4.clear();
         }
-        fstream Bfile(path + filen + ".txt");
-        if (Bfile.fail())
+        cout << endl << "Welcome to Scrabble Junior!" << endl;
+        cout << "What's the name of the Board File (without .txt) : ";
+        while(true)
         {
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << endl << "Invalid File Name!" << endl;
-            cout << "What's the name of the Board File (without .txt) : ";
+            cin >> filen;
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << endl << "Invalid File Name!" << endl;
+                cout << "What's the name of the Board File (without .txt) : ";
+            }
+            fstream Bfile(path + filen + ".txt");
+            if (Bfile.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << endl << "Invalid File Name!" << endl;
+                cout << "What's the name of the Board File (without .txt) : ";
+            }
+            else
+                break;
         }
-        else
-            break;
-    }
-    sb.OpenBoard(filen);
-    sb.DrawGameBoard(text_color);
-    pool.InitialPool(sb.gameboard, sb.boardsize);
-    plr.GetPlayers();
-    plr.GetIA();
-    int cn = 1;
-    while(true)
-    {
-        cout << endl << "Do you want to customize your names? [0 for No || Any other number for Yes] ";
-        cin >> cn;
-        if(cin.fail())
-        {
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << endl << "Invalid Answer (it has to be a Number)! " << endl;
-        }
-        else
-            break;
-    }
-
-    if (cn != 0)
-        plr.CustomPlayerNames();
-
-    if (pool.pool.size() >= 7 * plr.nplayers)
-        initiald = 7;
-    else
-        initiald = pool.pool.size() / plr.nplayers;
-    pool.InitialDraw(initiald, plr.nplayers);
-    srand(time(0));
-    turn = rand() % plr.nplayers + 1;
-    string playername;
-    vector <char> playerpool, adversariepool;
-    int playerscore = 0;
-    bool isia = false;
-    while (playing)
-    {
-        if (turn < plr.nplayers)
-            turn ++;
-        else
-            turn = 1;
-        turncount = 0;
-        playerscore = 0;
-        switch (turn)
-        {
-            case 1:
-            {
-                playerscore = plr.scorep1;
-                isia = plr.ia1;
-                playername = plr.pname1;
-                playerpool = pool.charp1;
-                adversariepool = pool.charp2;
-                break;
-            }
-            case 2:
-            {
-                playerscore = plr.scorep2;
-                isia = plr.ia2;
-                playername = plr.pname2;
-                playerpool = pool.charp2;
-                if (plr.nplayers >= 3)
-                    adversariepool = pool.charp3;
-                else
-                    adversariepool = pool.charp1;
-                break;
-            }
-            case 3:
-            {
-                playerscore = plr.scorep3;
-                isia = plr.ia3;
-                playername = plr.pname3;
-                playerpool = pool.charp3;
-                if (plr.nplayers >= 4)
-                    adversariepool = pool.charp4;
-                else
-                    adversariepool = pool.charp1;
-                break;
-            }
-            case 4:
-            {
-                playerscore = plr.scorep4;
-                isia = plr.ia4;
-                playername = plr.pname4;
-                playerpool = pool.charp4;
-                adversariepool = pool.charp1;
-                break;
-            }
-        }
-
-        cout << endl << "------------  " << playername << " TURN  " << "------------" << endl;
-        cout << playername << " ---> SCORE: " << playerscore << endl;
-        cout << "scorechips: " << sb.scorechips << endl;
+        sb.OpenBoard(filen);
         sb.DrawGameBoard(text_color);
-        pool.WritePlrPool(playerpool, playername);
-        while (turncount < 2 && sb.PlayPossible(playerpool))
+        pool.InitialPool(sb.gameboard, sb.boardsize);
+        plr.GetPlayers();
+        plr.GetIA();
+        int cn = 1;
+        while(true)
         {
-            validp = false;
+            cout << endl << "Do you want to customize your names? [0 for No || Any other number for Yes] ";
+            cin >> cn;
+            if(cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << endl << "Invalid Answer (it has to be a Number)! " << endl;
+            }
+            else
+                break;
+        }
+
+        if (cn != 0)
+            plr.CustomPlayerNames();
+
+        if (pool.pool.size() >= 7 * plr.nplayers)
+            initiald = 7;
+        else
+            initiald = pool.pool.size() / plr.nplayers;
+        pool.InitialDraw(initiald, plr.nplayers);
+        srand(time(0));
+        turn = rand() % plr.nplayers + 1;
+        string playername;
+        vector <char> playerpool, adversariepool;
+        int playerscore = 0;
+        bool isia = false;
+        while (playing)
+        {
+            if (turn < plr.nplayers)
+                turn ++;
+            else
+                turn = 1;
+            turncount = 0;
+            playerscore = 0;
+            switch (turn)
+            {
+                case 1:
+                {
+                    playerscore = plr.scorep1;
+                    isia = plr.ia1;
+                    playername = plr.pname1;
+                    playerpool = pool.charp1;
+                    adversariepool = pool.charp2;
+                    break;
+                }
+                case 2:
+                {
+                    playerscore = plr.scorep2;
+                    isia = plr.ia2;
+                    playername = plr.pname2;
+                    playerpool = pool.charp2;
+                    if (plr.nplayers >= 3)
+                        adversariepool = pool.charp3;
+                    else
+                        adversariepool = pool.charp1;
+                    break;
+                }
+                case 3:
+                {
+                    playerscore = plr.scorep3;
+                    isia = plr.ia3;
+                    playername = plr.pname3;
+                    playerpool = pool.charp3;
+                    if (plr.nplayers >= 4)
+                        adversariepool = pool.charp4;
+                    else
+                        adversariepool = pool.charp1;
+                    break;
+                }
+                case 4:
+                {
+                    playerscore = plr.scorep4;
+                    isia = plr.ia4;
+                    playername = plr.pname4;
+                    playerpool = pool.charp4;
+                    adversariepool = pool.charp1;
+                    break;
+                }
+            }
+
+            cout << endl << "------------  " << playername << " TURN  " << "------------" << endl;
+            cout << playername << " ---> SCORE: " << playerscore << endl;
+            cout << "scorechips: " << sb.scorechips << endl;
             sb.DrawGameBoard(text_color);
             pool.WritePlrPool(playerpool, playername);
-            while (playing && !validp)
+            while (turncount < 2 && sb.PlayPossible(playerpool))
             {
-                if (isia)
-                    sb.IAPlayer(playerpool, adversariepool, line, col);
-                else
+                validp = false;
+                sb.DrawGameBoard(text_color);
+                pool.WritePlrPool(playerpool, playername);
+                while (playing && !validp)
                 {
-                    scoord = sb.StringCoord();
-                    line = sb.GetLine(scoord);
-                    col = sb.GetCol(scoord);
+                    if (isia)
+                        sb.IAPlayer(playerpool, adversariepool, line, col);
+                    else
+                    {
+                        scoord = sb.StringCoord();
+                        line = sb.GetLine(scoord);
+                        col = sb.GetCol(scoord);
+                    }
+                    if (sb.ValidLetter(line, col, playerpool))
+                    {
+                        playerscore += sb.TurnScore(line, col, 2, true);
+                        sb.playedl[line][col] = '1';
+                        turncount += 1;
+                        validp = true;
+                    }
+                    else
+                        cout << endl << "Invalid Letter for the current Board Layout!" << endl;
                 }
-                if (sb.ValidLetter(line, col, playerpool))
-                {
-                    playerscore += sb.TurnScore(line, col, 2, true);
-                    sb.playedl[line][col] = '1';
-                    turncount += 1;
-                    validp = true;
-                }
-                else
-                    cout << endl << "Invalid Letter for the current Board Layout!" << endl;
-            }
-        }
-
-        if (turncount != 0)
-        {
-            pool.DrawPoolTurn(playerpool, min(turncount, pool.pool.size()));
-        }
-        else
-        {
-            if (pool.pool.size() > 0)
-                pool.ExchangeTiles(playerpool);
-        }
-        switch (turn)
-        {
-            case 1:
-            {
-                plr.scorep1 = playerscore;
-                pool.charp1 = playerpool;
-                break;
-            }
-            case 2:
-            {
-                plr.scorep2 = playerscore;
-                pool.charp2 = playerpool;
-                break;
-            }
-            case 3:
-            {
-                plr.scorep3 = playerscore;
-                pool.charp3 = playerpool;
-                break;
-            }
-            case 4:
-            {
-                plr.scorep4 = playerscore;
-                pool.charp4 = playerpool;
-                break;
-            }
             }
 
-        if (sb.scorechips == 0)
-            playing = false;
+            if (turncount != 0)
+            {
+                pool.DrawPoolTurn(playerpool, min(turncount, pool.pool.size()));
+            }
+            else
+            {
+                if (pool.pool.size() > 0)
+                    pool.ExchangeTiles(playerpool);
+            }
+            switch (turn)
+            {
+                case 1:
+                {
+                    plr.scorep1 = playerscore;
+                    pool.charp1 = playerpool;
+                    break;
+                }
+                case 2:
+                {
+                    plr.scorep2 = playerscore;
+                    pool.charp2 = playerpool;
+                    break;
+                }
+                case 3:
+                {
+                    plr.scorep3 = playerscore;
+                    pool.charp3 = playerpool;
+                    break;
+                }
+                case 4:
+                {
+                    plr.scorep4 = playerscore;
+                    pool.charp4 = playerpool;
+                    break;
+                }
+            }
+
+            if (sb.scorechips == 0)
+                playing = false;
+        }
+        vector <int> points;
+        points.push_back(plr.scorep1);
+        points.push_back(plr.scorep2);
+        if (plr.nplayers >= 3)
+            points.push_back(plr.scorep3);
+        if (plr.nplayers >= 4)
+            points.push_back(plr.scorep4);
+        sort(points.begin(), points.end());
+        cout << endl << "Points: ";
+        for (int i = 0; i < points.size(); i++)
+        {
+            cout << points[i] << "   ";
+        }
+        cout << endl << endl << "END";
+
+        while(true)
+        {
+            cout << endl << "Do you want to Restart? [0 for No || Any other number for Yes] ";
+            cin >> rranswer;
+            if(cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << endl << "Invalid Answer (it has to be a Number)! " << endl;
+            }
+            else
+                break;
+        }
+        if (rranswer == 0)
+            rr = false;
+        rranswer = 0;
     }
-    vector <int> points;
-    points.push_back(plr.scorep1);
-    points.push_back(plr.scorep2);
-    if (plr.nplayers >= 3)
-        points.push_back(plr.scorep3);
-    if (plr.nplayers >= 4)
-        points.push_back(plr.scorep4);
-    sort(points.begin(), points.end());
-    cout << endl << "Points: ";
-    for (int i = 0; i < points.size(); i++)
-    {
-        cout << points[i] << "   ";
     }
-    cout << endl << endl << "END";
-}
