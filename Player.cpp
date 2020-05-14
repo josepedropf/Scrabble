ebfd453465ce78e5dd8608a03af8e4bcf5b891a5
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-14 15:23:00 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -72,3 +72,132 @@ void Player::CustomPlayerNames()
     }
 }
 
+void Player::GetIA()
+{
+    int iaanswer = 0;
+
+    switch (nplayers)
+    {
+        case 2:
+        {
+            while (true)
+            {
+                cout << endl << "Input 0 for no IA Players" << endl;
+                cout << "Input 1 for 1 Normal vs 1 IA" << endl;
+                cout << "Input 2 for 2 IA Players" << endl;
+                cin >> iaanswer;
+                if (cin.fail() || iaanswer < 0 || iaanswer > 2)
+                {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << endl << "Invalid input!" << endl;
+                }
+                else
+                    break;
+            }
+            break;
+        }
+        case 3:
+        {
+            while (true)
+            {
+                cout << endl << "Input 0 for no IA Players" << endl;
+                cout << "Input 3 for 2 Normal vs 1 IA" << endl;
+                cout << "Input 4 for 1 Normal vs 2 IA" << endl;
+                cout << "Input 5 for 3 IA Players" << endl;
+                cin >> iaanswer;
+                if (cin.fail() || iaanswer < 0 || iaanswer > 5)
+                {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << endl << "Invalid input!" << endl;
+                }
+                else
+                    break;
+            }
+            break;
+        }
+        case 4:
+        {
+            while (true)
+            {
+                cout << endl << "Input 0 for no IA Players" << endl;
+                cout << "Input 6 for 3 Normal vs 1 IA" << endl;
+                cout << "Input 7 for 2 Normal vs 2 IA" << endl;
+                cout << "Input 8 for 1 Normal vs 3 IA" << endl;
+                cout << "Input 9 for 4 IA Players" << endl;
+                cin >> iaanswer;
+                if (cin.fail() || iaanswer < 0 || iaanswer > 5)
+                {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << endl << "Invalid input!" << endl;
+                }
+                else
+                    break;
+            }
+            break;
+        }
+    }
+    switch (iaanswer)
+    {
+        case 0:
+            break;
+        case 1:
+        {
+            ia2 = true;
+            break;
+        }
+        case 2:
+        {
+            ia1 = true;
+            ia2 = true;
+            break;
+        }
+        case 3:
+        {
+            ia3 = true;
+            break;
+        }
+        case 4:
+        {
+            ia2 = true;
+            ia3 = true;
+            break;
+        }
+        case 5:
+        {
+            ia1 = true;
+            ia2 = true;
+            ia3 = true;
+            break;
+        }
+        case 6:
+        {
+            ia4 = true;
+            break;
+        }
+        case 7:
+        {
+            ia3 = true;
+            ia4  = true;
+            break;
+        }
+        case 8:
+        {
+            ia2 = true;
+            ia3 = true;
+            ia4 = true;
+            break;
+        }
+        case 9:
+        {
+            ia1 = true;
+            ia2 = true;
+            ia3 = true;
+            ia4 = true;
+            break;
+        }
+    }
+}
+
