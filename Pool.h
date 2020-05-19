6d3b468d8508b6a1feed7c3b21655c74572f8d2f
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-19 15:38:56 +0100

    Update Pool.h

--- Pool.h
+++ Pool.h
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <vector>
 #ifndef SCRABBLEJUNIOR_POOL_H
@@ -8,11 +9,11 @@ class Pool {
 public:
     vector <char> pool, charp1, charp2, charp3, charp4;
 
-    void InitialPool(vector<vector <char>> gameboard, int boardsize);
+    void InitialPool(vector<vector <char>> gameboard, int boardsize, unsigned int color1, unsigned int color2);
     // Gets the characters from the Game Board in order to make the initial letter pool
     char DrawRandomChar();
     void InitialDraw(int ndraw, int np);
-    void WritePlrPool(vector <char> plrpool, string pname);
+    void WritePlrPool(vector<char> plrpool, string pname, unsigned int color1, unsigned int color2);
     void DrawPoolTurn(vector <char> &plrpool, int drawcount);
     void ExchangeTiles(vector <char> &plrpool);
 };
