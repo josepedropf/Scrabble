9362b187bf47ae4a9020e25ccfe8f6bc9b3915d1
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-19 15:24:23 +0100

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -636,8 +636,6 @@ void Board::Coordinates(vector <vector<char>> &gameboard,
 void Board::GetWords(vector <string> &possible_words, vector <vector<char>> gameboard)
 {
     fstream wordfile;
-    //wordfile.open("C:\\Users\\Utilizador\\Desktop\\WORDS.TXT");
-    //wordfile.open("C:\\Users\\MSI\\Desktop\\WORDS.TXT");
     wordfile.open("WORDS.TXT");
     string content;
 
