b1d124a10dd3eece19933dca84e309b0bd518ab1
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-20 15:58:13 +0100

    Update ScrabbleJunior.cpp

--- ScrabbleJunior.cpp
+++ ScrabbleJunior.cpp
@@ -35,15 +35,15 @@ int main()
     Player plr;
     ScrabbleBoard sb;
     string filen;
-    unsigned int mtextcolor = 1, stextcolor = 2;
+    unsigned int mtextcolor = 1, stextcolor = 2; // Main Color and Secondary Color
     int turn, turncount = 0, rranswer = 1;
     int initiald = 7, line = 0, col = 0;
     string scoord;
-    bool playing = true, validp, rr = true, forceend = false;
+    bool playing = true, validp = false, rr = true, forceend = false;
     sb.SetColor(mtextcolor);
     while (rr)
     {
-        if (rranswer == 0)
+        if (rranswer == 0) // Variables reseted when user restarts the game
         {
             plr.scorep1 = plr.scorep2 = plr.scorep3 = plr.scorep4 = 0;
             sb.gameboard.clear();
@@ -60,7 +60,7 @@ int main()
         }
         sb.Logo();
         cout << endl << "What's the path or name of the Board File (without .txt) : ";
-        while(true)
+        while (true)
         {
             cin >> filen;
             if (cin.fail())
@@ -81,14 +81,18 @@ int main()
             else
                 break;
         }
+        // Retrieving all the information from the Board File (boardsize, gameboard, words)
         sb.OpenBoard(filen);
         sb.DrawGameBoard(mtextcolor, stextcolor);
+        // Creating the initial Tile Pool with the characters from the words on the board
         pool.InitialPool(sb.gameboard, sb.boardsize, mtextcolor, stextcolor);
+        // Asking if the user wants to customize the colors and customizing if he does
         sb.CustomizeColor(mtextcolor, stextcolor);
+        // Asking how many players the user wants and which are IA and Normal Human Players
         plr.GetPlayers();
         plr.GetIA();
         int cn = 1;
-        while(true)
+        while (true)  // Name Customization
         {
             cout << endl << "Do you want to customize your names? [0 for No || Any other number for Yes] ";
             cin >> cn;
@@ -101,17 +105,18 @@ int main()
             else
                 break;
         }
-
+        
         if (cn != 0)
             plr.CustomPlayerNames();
-
+        // Checking if there are enough tiles to give 7 to each player
+        // If there aren't, distribution is recalibrated in a way that assures each player gets the same number of tiles
         if (pool.pool.size() >= 7 * plr.nplayers)
             initiald = 7;
         else
-            initiald = pool.pool.size() / plr.nplayers;
+            initiald = pool.pool.size() / plr.nplayers; 
         pool.InitialDraw(initiald, plr.nplayers);
         srand(time(0));
-        turn = rand() % plr.nplayers + 1;
+        turn = rand() % plr.nplayers + 1; // Deciding randomly who is the first
         string playername;
         vector <char> playerpool, adversariepool;
         int playerscore = 0;
@@ -119,20 +124,21 @@ int main()
         while (playing)
         {
             if (turn < plr.nplayers)
-                turn ++;
+                turn ++; // Passing to the next turn before everything else
             else
                 turn = 1;
             turncount = 0;
             playerscore = 0;
-            switch (turn)
+            // Reseting all turn exclusive variables
+            switch (turn) // Variables changing to match the player who is about to play it's turn
             {
                 case 1:
                 {
-                    playerscore = plr.scorep1;
-                    isia = plr.ia1;
-                    playername = plr.pname1;
-                    playerpool = pool.charp1;
-                    adversariepool = pool.charp2;
+                    playerscore = plr.scorep1;      // Score
+                    isia = plr.ia1;                 // IA Bool (true if player is IA)
+                    playername = plr.pname1;        // Name
+                    playerpool = pool.charp1;       // Player's Pool 
+                    adversariepool = pool.charp2;   // Pool of the next player to play (used by IA Players)
                     break;
                 }
                 case 2:
@@ -182,18 +188,20 @@ int main()
             cout << playerscore << endl;
             sb.SetColor(mtextcolor);
             cout << "scorechips: " << sb.scorechips << endl;
+            // Turn Beggining 
             while (turncount < 2 && sb.PlayPossible(playerpool))
             {
                 validp = false;
                 sb.DrawGameBoard(mtextcolor, stextcolor);
                 pool.WritePlrPool(playerpool, playername, mtextcolor, stextcolor);
+                // Writing current Pool and Board
                 while (playing && !validp)
                 {
                     if (isia)
-                        sb.IAPlayer(playerpool, adversariepool, line, col);
+                        sb.IAPlayer(playerpool, adversariepool, line, col); // IA Player Move 
                     else
                     {
-                        scoord = sb.StringCoord(forceend, playing);
+                        scoord = sb.StringCoord(forceend, playing); // Human Player inputs Coordinates
                         if (forceend)
                         {
                             turncount = 3;
@@ -202,7 +210,7 @@ int main()
                         line = sb.GetLine(scoord);
                         col = sb.GetCol(scoord);
                     }
-                    if (sb.ValidLetter(line, col, playerpool))
+                    if (sb.ValidLetter(line, col, playerpool))  // If the Move is valid, it is implemented
                     {
                         playerscore += sb.TurnScore(line, col, true);
                         sb.playedl[line][col] = '1';
@@ -211,13 +219,13 @@ int main()
                     }
                     else
                     {
-                        if (!forceend)
+                        if (!forceend)  // Force End ends the game directly
                             cout << endl << "Invalid Letter for the current Board Layout!" << endl;
                     }
                 }
             }
 
-            if (turncount != 0 && !forceend)
+        if (turncount != 0 && !forceend)    // If at least one tile was played, the player draws tiles from the main pool
             {
                 pool.DrawPoolTurn(playerpool, min(turncount, pool.pool.size()));
             }
@@ -226,7 +234,7 @@ int main()
                 if (!pool.pool.empty())
                     pool.ExchangeTiles(playerpool);
             }
-            switch (turn)
+            switch (turn)   // Saving the changes made during the turn with turn exclusive variables
             {
                 case 1:
                 {
@@ -254,16 +262,17 @@ int main()
                 }
             }
 
-            if (sb.scorechips == 0)
-                playing = false;
+            if (sb.scorechips == 0) // The game ends when all score chips have been assigned to the players
+                playing = false;    // The variable scorechips counts the unassigned score chips
         }
 
-        if (!forceend)
+        if (!forceend)  // The endgame statistics are shown everytime the program ends naturally
         {
             string fplayername, winnername;
             bool draw = false;
             vector <string> places = {"First Place:  ", "Second Place: ", "Third Place:  ", "Fourth Place: "};
             vector <vector<int>> points;
+            // Associating the players with their respective points
             plr.fscorep1 = {1, plr.scorep1};
             plr.fscorep2 = {2, plr.scorep2};
             plr.fscorep3 = {3, plr.scorep3};
@@ -277,18 +286,18 @@ int main()
 
             sort(points.begin(), points.end(), [] (const vector <int> &v1, const vector <int> &v2)
             {
-                return v1[1] > v2[1];
+                return v1[1] > v2[1]; // Sorting the Players by their respective points
             });
 
-            for (int i = 0; i <points.size() - 1; i++)
+            for (int i = 0; i <points.size() - 1; i++) // Handling of position ties
             {
-                if (points[i][1] == points[i + 1][1])
-                    places[i + 1] = places[i];
+                if (points[i][1] == points[i + 1][1])   
+                    places[i + 1] = places[i];          // Giving the highest position to tied players
             }
 
-            for (int i = 0; i < points.size(); i++)
+            for (int i = 0; i < points.size(); i++) // Showing everyone's positions and points next to their names
             {
-                switch (points[i][0])
+                switch (points[i][0]) // Selecting the names using the order of the points sorted before
                 {
                     case 1:
                     {
@@ -312,7 +321,7 @@ int main()
                     }
                 }
 
-                if (i == 0)
+                if (i == 0) // Saving the name of the winner to use in the Winner Message
                     winnername = fplayername;
                 cout << endl << places[i];
                 sb.SetColor(11);
@@ -325,9 +334,9 @@ int main()
                 cout << " Points." << endl;
             }
 
-            draw = places[0] == places[1];
+            draw = places[0] == places[1]; // There is no winner when the first and second have the same points
 
-            if (!draw)
+            if (!draw)  // When there is a winner, the Winner Message appears
             {
                 cout << endl << "---------------  ";
                 sb.SetColor(11);
@@ -335,13 +344,13 @@ int main()
                 sb.SetColor(mtextcolor);
                 cout << " WINS!!!" << "  ---------------" << endl;
             }
-            else
+            else        // When the game ends tied, the Draw Message appears
                 cout << endl << "---------------  " << "DRAW" << "  ---------------" << endl;
         }
 
         cout << endl << "END";
 
-        while(true)
+        while (true)    // Asking the user if he wishes to restart the game
         {
             cout << endl << "Do you want to Restart? [0 for No || Any other number for Yes] ";
             cin >> rranswer;
@@ -354,8 +363,11 @@ int main()
             else
                 break;
         }
-        if (rranswer == 0)
+        if (rranswer == 0)  // If the input is 0, the program closes
+        {
             rr = false;
-        rranswer = 0;
+            cout << endl << "END";
+        }
+        rranswer = 0;   
     }
 }
