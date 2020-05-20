c16af7e6291c41a37cb6334789a3af22b7255f64
José Pedro Peixoto Ferreira <55797452+josepedropf@users.noreply.github.com>
2020-05-20 16:14:53 +0100

    Update BoardBuilder.cpp

--- BoardBuilder.cpp
+++ BoardBuilder.cpp
@@ -4,6 +4,7 @@
 #include "Board.h"
 using namespace std;
 
+// The Descriptions of the Functions are on the Header Files
 int main()
 {
     int mode = 1;
@@ -16,15 +17,15 @@ int main()
     int boardsize = 0;
     Board board;
 
-    board.GetWords(possible_words, gameboard);
+    board.GetWords(possible_words, gameboard); // Transfers the words from the words.txt file to the vector possible_words
 
     while (!end)
     {
 
-        board.PickSize(boardsize);
+        board.PickSize(boardsize); // Asks and gets the size of the Board
         vector<char> line;
         line.clear();
-        gameboard.clear();
+        gameboard.clear();          // Reseting of variables for when the user wants to make more than one board
 
         for (int i = 0; i < boardsize; i++)
         {
@@ -33,16 +34,17 @@ int main()
 
         for (int i = 0; i < boardsize; i++)
         {
-            gameboard.push_back(line);
+            gameboard.push_back(line);  // Filling the Board with characters 0, already with it's actual size
         }
-
+        
         board.DrawBoardClean(boardsize, gameboard);
 
+        // Choosing Modes
         cout << endl << "Modes: " << endl << endl << "O -> Exit" << endl
              << "1 -> Choose the initial coordinates for Random Words" << endl << "2 -> Random Board" << endl
              << "3 -> Choose the initial Coordinates and the Words" << endl;
         cout << "Choose the mode of Board Building (Input one of the numbers above): ";
-        while (1)
+        while (true)    // Ensuring the mod is an integer between 0 and 3
         {
             cin >> mode;
             if (cin.fail() || mode < 0 || mode > 3)
@@ -59,7 +61,7 @@ int main()
                 break;
         }
 
-        switch (mode)
+        switch (mode)   // Calling the Board Building function correspondent to each mode
         {
             case 0:{
                 break;
@@ -78,7 +80,7 @@ int main()
             }
         }
         cout << "Do you want to save the Board in a File [Y for yes || N for no] ? ";
-        while(1)
+        while (true)    // Asking the user if he wishes to save the Board in a file and saving in the case he does
         {
             cin >> savefile;
             if (cin.fail())
@@ -95,9 +97,9 @@ int main()
                 {
                     string filename;
                     cout << "Name of the File (without .txt) : ";
-                    while(1)
+                    while (true)
                     {
-                        cin >> filename;
+                        cin >> filename;    // Giving the file a custom name
                         if (cin.fail())
                         {
                             cin.clear();
@@ -119,7 +121,7 @@ int main()
         }
 
         cout << "Do you want to make a different Board [Y for yes || N for no] ? ";
-        while(1)
+        while (true)    // Giving the user the possibility of doing more boards without exiting the program
         {
             cin >> rr;
             if (cin.fail())
@@ -136,7 +138,7 @@ int main()
                     break;
                 else
                 {
-                    end = true;
+                    end = true; // If the user wants to quit, the program ends
                     break;
                 }
             }
